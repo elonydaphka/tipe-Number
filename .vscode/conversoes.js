a1f99d7bee92c8d2cf1eb7e7972df0af9b1c1c3f
Daphka Elony <daphka.elony@safeweb.com.br>
2022-12-20 15:05:35 -0300

    first commit

---
+++ .vscode/conversoes.js
@@ -0,0 +1,15 @@
+
+// tipo de dado
+// booleanos
+
+// conversao implícita
+const numero = 456;
+const numeroString = "456";
+
+// Number()
+// String()
+console.log(numero == numeroString)
+console.log(numero + Number(numeroString))
+
+
+// conversão explícita
---
+++ .vscode/type-String.js
@@ -0,0 +1,13 @@
+const texto1 = "olá, mundo!"
+const texto2 = 'olá, mundo!';
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é";
+const meuNome = "Lonardo";
+console.log(citacao)
+
+// contatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string ou template literal
